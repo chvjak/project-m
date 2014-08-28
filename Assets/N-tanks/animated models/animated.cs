a9f68d0e8416fe142218c2406bbb4ab3fb2c41f5
Дмитрий Архипенко <chvjak@gmail.com>
2014-08-28 21:48:34 +0300

    Animation test

--- Assets/N-tanks/animated models/animated.cs
+++ Assets/N-tanks/animated models/animated.cs
@@ -2,23 +2,23 @@
 using System.Collections;
 
 public class animated : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		Animator a = GetComponent<Animator>();
-		a.Play("idle");
-	}
 	
+
 	// Update is called once per frame
 	void Update () {
 		Animator a = GetComponent<Animator>();
 
-		if (Input.GetKey("up"))
+		if (Input.GetKey("up")){
+			Debug.Log("driving");
 			a.Play("driving");
+		}
 		if (Input.GetKey("space"))
+		{
+			Debug.Log("firing");
 			a.Play("firing");
-		//if (Input.GetKey("d"))
-			//a.Play("destroyed");
+		}
+		if (Input.GetKey("d"))
+			a.Play("destroyed");
 
 	}
 }
